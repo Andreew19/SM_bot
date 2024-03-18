9181b7ae2043eaaf8b5b236dc9252df7a0943f93
Andreew19 <andrew_04@list.ru>
2024-03-18 13:56:20 +0400

    bug fix

--- bot.py
+++ bot.py
@@ -1,22 +1,37 @@
 import os
 from dotenv import load_dotenv
-
+import psutil  
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
-from data import hdd,cpu_load,memory_usage
 
 load_dotenv()
 
 bot = Bot(os.environ['TOKEN'])
 dp = Dispatcher(bot)
 
-data = [f"CPU: {cpu_load}%",f"Memory: {memory_usage}%","Total: %.2f Gb" % (hdd.total / (2**30)),"Used: %.2f Gb" % (hdd.used / (2**30)),"Free: %.2f Gb" % (hdd.free / (2**30))]
-parss_data = "\n".join(data) 
 
 
 @dp.message_handler(CommandStart())
 async def send_welcome(message: types.Message):
+  
+ 
+  def get_cpu_load(): 
+    return psutil.cpu_percent(interval=1) 
+ 
+  def get_memory_usage(): 
+    mem = psutil.virtual_memory() 
+    return mem.percent 
+ 
+ 
+  cpu_load = get_cpu_load() 
+  memory_usage = get_memory_usage() 
+ 
+  hdd = psutil.disk_usage('/') 
+
+  data = [f"CPU: {cpu_load}%",f"Memory: {memory_usage}%","Total: %.2f Gb" % (hdd.total / (2**30)),"Used: %.2f Gb" % (hdd.used / (2**30)),"Free: %.2f Gb" % (hdd.free / (2**30))]
+  parss_data = "\n".join(data) 
+ 
   await message.answer(f"*Server load:*\n\n {parss_data}", parse_mode='markdown')
 
 
