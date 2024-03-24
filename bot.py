94955caf91c35e9373f1a3188b27391c2d56ee3d
Andreew19 <andrew_04@list.ru>
2024-03-24 14:38:16 +0400

    Stopped tracking env.local, and env.staging

--- bot.py
+++ bot.py
@@ -33,14 +33,14 @@ async def send_welcome(message: types.Message):
   def get_memory_usage(): 
     mem = psutil.virtual_memory() 
     return mem.percent 
- 
+  
  
   cpu_load = get_cpu_load() 
   memory_usage = get_memory_usage() 
  
   hdd = psutil.disk_usage('/') 
 
-  data = [f"CPU: {cpu_load}%",f"Memory: {memory_usage}%","Total: %.2f Gb" % (hdd.total / (2**30)),"Used: %.2f Gb" % (hdd.used / (2**30)),"Free: %.2f Gb" % (hdd.free / (2**30))]
+  data = [f"CPU: {cpu_load}%",f"Load Average: {psutil.getloadavg()}",f"Memory: {memory_usage}%","Total: %.2f Gb" % (hdd.total / (2**30)),"Used: %.2f Gb" % (hdd.used / (2**30)),"Free: %.2f Gb" % (hdd.free / (2**30))]
   parss_data = "\n".join(data) 
  
   await message.answer(f"*Server load:*\n\n {parss_data}", parse_mode='markdown', reply_markup=start_keyboard)
